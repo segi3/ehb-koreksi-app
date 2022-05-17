dcc2f95a3488eb1746c37ca717c3415d164b7aa1
rafi nizar <rafi.nizar15@gmail.com>
2022-05-17 12:38:36 +0700

    home dashboard blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,47 +15,46 @@
                 <div class="col-lg-2">
                     Paket Ujian
                 </div>
-                <div class="col-lg-3" id="paket-selector">
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected disabled>Open this select menu</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                <div class="col-lg-5" id="paket-selector">
+                    <select class="form-select" aria-label="Default select example" id="select-paket">
+                        <option selected disabled>Pilih ujian</option>
                       </select>
+                </div>
+                <div class="offset-lg-4 col-lg-1">
+                    <button type="button" class="btn btn-secondary" id="refresh-button">Refresh</button>
                 </div>
             </div>
 
         </div>
     </div>
 
-    <table class="table table-dark" id ="table_id">
+    <div class="alert alert-primary" role="alert" id="koreksi-alert" style="display:none">
+        This is a primary alert—check it out!
+    </div>
+
+    <table class="table table-dark" id ="ujian_siswa_table">
         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">NISN</th>
+            <th scope="col">Nama Siswa</th>
+            <th scope="col">Sekolah</th>
+            <th scope="col">Jurusan</th>
+            <th scope="col">Nilai</th>
+            <th scope="col">Predikat</th>
+            <th scope="col">Status Koreksi</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {{-- <tr>
+            <th scope="row">14045</th>
+            <td>Rafi Nizar</td>
+            <td>SMA INTI</td>
+            <td>MIPA</td>
+            <td>80</td>
+            <td>BAGUS</td>
+            <td>SELESAI DIKOREKSI</td>
+          </tr> --}}
+
         </tbody>
       </table>
 @stop
@@ -67,13 +66,101 @@
 @section('js')
 
     <script src="{{ asset('js/components/load-siswa.js')}}"></script>
+    <script src="{{ asset('js/components/ujian-siswa-table-loader.js')}}"></script>
 
     <script>
     $(document).ready( function () {
-        $('#table_id').DataTable( {
-            "lengthMenu": [[1, 10, 25, 50, -1], [1, 10, 25, 50, "All"]],
-            "paging": true
-        });
+
+        var tableUjian = $('#ujian_siswa_table').DataTable({
+            language: {
+                "emptyTable": " ",
+                "zeroRecords": " ",
+                'loadingRecords': '&nbsp;',
+                'processing': 'Loading...'
+            },
+            retrieve: true,
+            data: {},
+            columns: [
+                { data: 'nisn' },
+                { data: 'nama_siswa' },
+                { data: 'nama_sekolah' },
+                { data: 'jurusan' },
+                { data: 'jumlah_benar',
+                    render: function ( data, type, row ) {
+                        var nilai = parseInt(data)
+                        if (nilai == -2 || nilai == '-2') {
+                            nilai = '-'
+                        }
+                        return nilai;
+                    }
+                },
+                { data: 'predikat' },
+                { data: 'jumlah_benar',
+                    render: function ( data, type, row ) {
+                        var nilai = 'SUDAH DIKOREKSI'
+                        if (parseInt(data) == -2) {
+                            nilai = 'BELUM DIKOREKSI'
+                        }
+                        return nilai;
+                    }
+                }
+            ]
+        })
+
+        $.ajax({
+            type: 'GET',
+            contentType: "application/json",
+            dataType: "json",
+            url: 'http://127.0.0.1:8000/api/is-any-onprogress',
+            success: (data) => {
+
+                tableUjian.clear()
+                tableUjian.draw()
+
+                if (data.data.on_progress) {
+                    $('#koreksi-alert').html('Sedang melakukan koreksi pada ' + data.data.nama)
+                    $('#koreksi-alert').show();
+
+                    var row = document.createElement("tr");
+                    var msg = document.createElement('td');
+                    msg.innerHTML = 'Tidak bisa melihat data saat sedang melakukan koreksi ///'
+                    msg.setAttribute("colspan", "100")
+                    msg.setAttribute('style', 'text-align:center;')
+                    row.append(msg)
+                    $('#ujian_siswa_table').append(row);
+                } else {
+                    var row = document.createElement("tr");
+                    var msg = document.createElement('td');
+                    msg.innerHTML = 'Silahkan pilih jadwal ujian ///'
+                    msg.setAttribute("colspan", "100")
+                    msg.setAttribute('style', 'text-align:center;')
+                    row.append(msg)
+                    $('#ujian_siswa_table').append(row);
+                }
+            },
+            error: (err) => {
+                console.log(err)
+            }
+        })
+
+        // fetch all active jadwal
+        $.ajax({
+            type: 'GET',
+            url: 'http://127.0.0.1:8000/api/active-jadwal',
+            success: (data) => {
+                const ujian = data.data
+
+                ujian.forEach(el => {
+                    var option = document.createElement("option")
+                    option.value = el.jadwal_ujian_id
+                    option.innerHTML = el.nama + ' sesi ' + el.sesi
+                    $('#select-paket').append(option);
+                })
+            },
+            error: (err) => {
+                console.log(err)
+            }
+        })
     } );
     </script>
 
